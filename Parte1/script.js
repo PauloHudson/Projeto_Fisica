2247976baeee81139fceae0cf57c7365c276d60d
Hugo <hugoen304@gmail.com>
2024-05-05 17:00:30 -0300

    graficos de distribucao de probabilidade implementados

--- Parte1/script.js
+++ Parte1/script.js
@@ -96,25 +96,23 @@ function processResults() {
     `;
 
 // Obtém uma referência para o elemento canvas
-var ctx = document.getElementById('meuGrafico').getContext('2d');
+var ctx1 = document.getElementById('grafio-de-onda-quantica').getContext('2d');
 
 // Gera os pontos da função seno
 var labels = [];
 var data = [];
-
-console.log("teste");
-console.log("L:", variaveis[0]);
-console.log("ni:", variaveis[1]);
+var data2 = [];
 
 for (var x = 0; x <= L; x = x + L/1000) {
-    var psi = ((2 * Math.PI * x) / L); // Converte graus para radianos
+    var psi = ((ni * Math.PI * x) / L); // Converte graus para radianos
     var y = Math.sqrt(ni / L) * Math.sin(psi); // Função seno de x
     labels.push(x);
     data.push(y);
+    data2.push(y*y);
 }
 
 // Define os dados do gráfico
-var chartData = {
+var chartData1 = {
     labels: labels,
     datasets: [{
         label: 'Função de Onda Quântica (ψ(x))',
@@ -126,7 +124,7 @@ var chartData = {
 };
 
 // Configurações do gráfico
-var options = {
+var options1 = {
     scales: {
         xAxes: [{
             scaleLabel: {
@@ -144,19 +142,58 @@ var options = {
 };
 
 // Cria o gráfico de linha
-var myChart = new Chart(ctx, {
+var myChart = new Chart(ctx1, {
     type: 'line',
-    data: chartData,
-    options: options
+    data: chartData1,
+    options: options1
+});
+
+/////////////////////////////////
+
+// Obtém uma referência para o elemento canvas
+var ctx2 = document.getElementById('grafio-de-distribuicao-de-probabilidade').getContext('2d');
+
+// Define os dados do gráfico
+var chartData2 = {
+    labels: labels,
+    datasets: [{
+        label: 'Distribuição de probabilidade (|ψ(x)|²)',
+        data: data2,
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1
+    }]
+};
+
+// Configurações do gráfico
+var options2 = {
+    scales: {
+        xAxes: [{
+            scaleLabel: {
+                display: true,
+                labelString: 'Ângulo (graus)'
+            }
+        }],
+        yAxes: [{
+            scaleLabel: {
+                display: true,
+                labelString: 'Valor de sen(1)'
+            }
+        }]
+    }
+};
+
+// Cria o gráfico de linha
+var myChart = new Chart(ctx2, {
+    type: 'line',
+    data: chartData2,
+    options: options2
 });
 }
 
 
 
-/////////////////////////////////
-
-
-
+////////////////////////////////
 
 document.getElementById('inputForm')?.addEventListener('submit', function(event) {
     event.preventDefault();
